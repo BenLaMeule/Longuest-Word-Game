d16baac015aaeaf8af96204de6d5387cbd018c80
Benjamin Meuleman <benjamin.meuleman@gmail.com>
2016-05-09 17:02:47 +0200

    solution with POST instead of GET

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'game', to: 'pages#game'
-  get 'score', to: 'pages#score'
+  post 'score', to: 'pages#score'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
